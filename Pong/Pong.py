662fb57ab725e5325a87cc26be8a531fb70b818e
ArcaneZephyr <ngwaikeet@gmail.com>
2020-01-07 00:54:11 +0800

    WIP Pong
    Used the same agent as space invaders with new epsilon decay

---
+++ Pong/Pong.py
@@ -0,0 +1,67 @@
+import gym
+from dddql_torch import DeepQNetwork, Agent
+import numpy as np
+from gym import wrappers
+
+"""
+TO DO:
+- implement a better epsilon decay
+
+gamma = discount
+alpha = learning rate
+"""
+
+if __name__ == '__main__':
+    env = gym.make('Pong-v0')
+    brain = Agent(gamma=0.90, epsilon=1.0, 
+                  alpha=0.004, maxMemorySize=5000)
+    while brain.memCntr < brain.memSize:
+        observation = env.reset()
+        done = False
+        while not done:
+            # 0 no action, 1 fire, 2 move right, 3 move left, 4 move right fire, 5 move left fire
+            action = env.action_space.sample()
+            observation_, reward, done, info = env.step(action)
+            brain.storeTransition(np.mean(observation[15:200,30:125], axis=2), action, reward,
+                                np.mean(observation_[15:200,30:125], axis=2))
+            observation = observation_
+    print('done initializing memory')
+
+    scores = []
+    epsHistory = []
+    numGames = 50000
+    batch_size=32
+    num_streak = 0
+    env = wrappers.Monitor(env, "tmp/pong-1", video_callable=lambda episode_id: True, force=True)
+    for i in range(numGames):
+        print('starting game ', i+1, 'epsilon: %.4f' % brain.EPSILON)
+        epsHistory.append(brain.EPSILON)
+        done = False
+        observation = env.reset()
+        frames = [np.sum(observation[15:200,30:125], axis=2)]
+        score = 0
+        lastAction = 0
+        while not done:
+            if len(frames) == 3:
+                action = brain.chooseAction(frames)
+                frames = []
+            else:
+                action = lastAction
+            observation_, reward, done, info = env.step(action)
+            score += reward
+            frames.append(np.sum(observation_[15:200,30:125], axis=2))
+            brain.storeTransition(np.mean(observation[15:200,30:125], axis=2), action, reward,
+                                  np.mean(observation_[15:200,30:125], axis=2))
+            observation = observation_
+            brain.learn(batch_size)
+            lastAction = action
+        scores.append(score)
+        if score > 0:
+            num_streak += 1
+        else:
+            num_streak = 0
+        
+        print('score:',score,' streak:',num_streak)
+
+        if num_streak > 10:
+            break
---
+++ Pong/dddql_torch.py
@@ -0,0 +1,97 @@
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import math
+import numpy as np
+
+class DeepQNetwork(nn.Module):
+    def __init__(self, ALPHA):
+        super(DeepQNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 8, stride=4, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.fc1 = nn.Linear(128*19*8, 512)
+        self.fc2 = nn.Linear(512, 6)
+        self.optimizer = optim.RMSprop(self.parameters(), lr=ALPHA)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, observation):
+        observation = T.Tensor(observation).to(self.device)
+        observation = observation.view(-1, 1, 185, 95)
+        observation = F.relu(self.conv1(observation))
+        observation = F.relu(self.conv2(observation))
+        observation = F.relu(self.conv3(observation))
+        observation = observation.view(-1, 128*19*8)
+        observation = F.relu(self.fc1(observation))
+        actions = self.fc2(observation)
+        return actions
+
+class Agent(object):
+    def __init__(self, gamma, epsilon, alpha,
+                 maxMemorySize, epsEnd=0.05,
+                 actionSpace=[0,1,2,3,4,5]):
+        self.GAMMA = gamma
+        self.EPSILON = epsilon
+        self.EPS_END = epsEnd
+        self.ALPHA = alpha
+        self.actionSpace = actionSpace
+        self.memSize = maxMemorySize
+        self.steps = 0
+        self.learn_step_counter = 0
+        self.memory = []
+        self.memCntr = 0
+        self.Q_eval = DeepQNetwork(alpha)
+        self.Q_next = DeepQNetwork(alpha)
+
+    def storeTransition(self, state, action, reward, state_):
+        if self.memCntr < self.memSize:
+            self.memory.append([state, action, reward, state_])
+        else:
+            self.memory[self.memCntr%self.memSize] = [state, action, reward, state_]
+        self.memCntr += 1
+
+    def chooseAction(self, observation):
+        rand = np.random.random()
+        actions = self.Q_eval.forward(observation)
+        if rand > self.EPSILON:
+            action = T.argmax(actions[1]).item()
+        else:
+            action = np.random.choice(self.actionSpace)
+        self.steps += 1
+        return action
+
+    def learn(self, batch_size):
+        #set gradient to zero for backpropagation
+        self.Q_eval.optimizer.zero_grad()
+
+        #select a batch
+        if self.memCntr+batch_size < self.memSize:
+            memStart = int(np.random.choice(range(self.memCntr)))
+        else:
+            memStart = int(np.random.choice(range(self.memSize-batch_size-1)))
+        miniBatch=self.memory[memStart:memStart+batch_size]
+        memory = np.array(miniBatch)
+
+        #get the q values for the predicted and next set of actions
+        Qpred = self.Q_eval.forward(list(memory[:,0][:])).to(self.Q_eval.device)
+        Qnext = self.Q_next.forward(list(memory[:,3][:])).to(self.Q_eval.device)
+
+        #update the reward value for the action in Qtarget
+        maxA = T.argmax(Qnext, dim=1).to(self.Q_eval.device)
+        rewards = T.Tensor(list(memory[:,2])).to(self.Q_eval.device)
+        Qtarget = Qpred
+        indices = np.arange(batch_size)
+        Qtarget[indices,maxA] = rewards + self.GAMMA*T.max(Qnext[1])
+
+        if self.memCntr > 5:
+            self.epsilon = max(0, min(0.9, 1.0 - math.log10((self.memCntr+1)/10)))
+
+        #get the loss with MSE and backpropagate it
+        loss = self.Q_eval.loss(Qtarget, Qpred).to(self.Q_eval.device)
+        loss.backward()
+        #step to move the environment forward
+        self.Q_eval.optimizer.step()
+        self.learn_step_counter += 1
